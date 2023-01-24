cd5b6fa211e6eec9e8cd69b83fab811736d49cfe
Paula <giannajuncal@gmail.com>
2023-01-24 18:36:10 +0000

    Diagrama

--- src/models.py
+++ src/models.py
@@ -8,23 +8,57 @@ from eralchemy2 import render_er
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
+class Usuario(Base):
+    __tablename__ = 'usuario'
     # Here we define columns for the table person
     # Notice that each column is also a normal Python instance attribute.
     id = Column(Integer, primary_key=True)
     name = Column(String(250), nullable=False)
+    user = Column(String(250), nullable=False)
+    email = Column(String(250), nullable=False)
 
-class Address(Base):
-    __tablename__ = 'address'
+class Characters(Base):
+    __tablename__ = 'characters'
+    # Here we define columns for the table person 
+    # Notice that each column is also a normal Python instance attribute.
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250), nullable=False)
+    gender = Column(String(250), nullable=False)
+    hairColor = Column(String(250), nullable=False)
+
+
+class Planets(Base):
+    __tablename__ = 'planets'
+    # Here we define columns for the table person 
+    # Notice that each column is also a normal Python instance attribute.
+    id = Column(Integer, primary_key=True)
+    Name = Column(String(250), nullable=False)
+    Population = Column(String(250), nullable=False)
+    Terrain = Column(String(250), nullable=False)
+    
+class Vehiculos (Base):
+    __tablename__ = 'vehiculos'
+    # Here we define columns for the table person 
+    # Notice that each column is also a normal Python instance attribute.
+    id = Column(Integer, primary_key=True)
+    nombre = Column(String(250), nullable=False)
+    crew = Column(String(250), nullable=False)
+    model = Column(String(250), nullable=False)
+    
+
+
+
+
+
+class Favorites(Base):
+    __tablename__ = 'favorites'
     # Here we define columns for the table address.
     # Notice that each column is also a normal Python instance attribute.
     id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+    usuario_id = Column(Integer, ForeignKey('usuario.id'))
+    planets_id = Column(Integer, ForeignKey('planets.id'))
+    characters_id = Column(Integer, ForeignKey('characters.id'))
+    vehiculos_id = Column(Integer, ForeignKey('vehiculos.id'))
 
     def to_dict(self):
         return {}
